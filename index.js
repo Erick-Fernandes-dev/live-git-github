f374031d85728cb7420eadc4a15a2e826eb311fc
Erick Fernandes <56647805+TBWA0002@users.noreply.github.com>
2020-08-26 09:05:11 -0300

    chore: Listagem de pessoas e seus cargos

--- index.js
+++ index.js
@@ -28,3 +28,6 @@ console.log(vect[1].nome + ", " + vect[2].nome + ", " + vect[3].nome);
 console.log(vect[1].idade + ", " + vect[2].idade + ", " + vect[3].idade);
 console.log(vect[1].altura + ", " + vect[2].altura + ", " + vect[3].altura);
 console.log(vect[1].cargo + ", " + vect[2].cargo + ", " + vect[3].cargo);
+
+console.log("\n")
+console.log("Fim da Listagem...")
