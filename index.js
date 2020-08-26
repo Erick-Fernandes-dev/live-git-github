eb6640e37c0dacf4acdc08e84fda36fd0f160288
Erick Fernandes <56647805+TBWA0002@users.noreply.github.com>
2020-08-26 08:54:42 -0300

    chore: Commit inicial

---
+++ index.js
@@ -0,0 +1,30 @@
+//Vetores
+
+const vect = [
+	{
+		nome: "Erick",
+		idade: 20,
+		altura: 1.75,
+		cargo: "Programador"
+	},
+
+	{
+		nome: "Adriano",
+		idade: 45,
+		altura: 1.56,
+		cargo: "Professor"
+	},
+
+	{
+		nome: "Maria",
+		idade: 50,
+		altura: 1.67,
+		cargo: "Advogada"
+	}
+
+]
+
+console.log(vect[1].nome + ", " + vect[2].nome + ", " + vect[3].nome);
+console.log(vect[1].idade + ", " + vect[2].idade + ", " + vect[3].idade);
+console.log(vect[1].altura + ", " + vect[2].altura + ", " + vect[3].altura);
+console.log(vect[1].cargo + ", " + vect[2].cargo + ", " + vect[3].cargo);
